80ed75cfaa88e39df077577c3259e299d54eb084
likearockets <aczech81@gmail.com>
2017-01-09 21:10:46 +0100

    Update zad_9.cpp

--- cw.9/zad_9.cpp
+++ cw.9/zad_9.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
 #include <conio.h>					
 #include <windows.h>
-#include <string>
 
 using namespace std;
 
 float x, y;
 int wybor, a = 1;
 
+
+void menu()
+{
+	cout << " | Kalkulator autorstwa Aleksandra Czecha |" << endl;
+	cout << " |        WYBIERZ RODZAJ DZIALANIA        |" << endl;
+	cout << " |            1 - Dodawanie               |" << endl;
+	cout << " |            2 - Odejmowanie             |" << endl;
+	cout << " |            3 - Mnozenie                |" << endl;
+	cout << " |            4 - Dzielenie               |" << endl;
+	cout << " |             0 - Wyjscie                |" << endl;
+}
+
+
+void wybory()
+{
+	switch (wybor)
+	{
+	case '1':
+		cout << "Wynik to: " << x + y << endl;
+		break;
+
+	case '2':
+		cout << "Wynik to: " << x - y << endl;
+		break;
+
+	case '3':
+		cout << "Wynik to: " << x * y << endl;
+		break;
+
+	case '4':
+		cout << "Wynik to: " << x / y << endl;
+		break;
+
+	case '0':
+		a = 2;
+		break;
+
+	default:
+		cout << "Wpisales zly numer" << endl;
+	}
+}
 
 
 int main()
@@ -19,51 +59,15 @@ int main()
 		cout << "Wybierz 2 liczbe: ";
 		cin >> y;
 
-		cout << " | Kalkulator autorstwa Aleksandra Czecha |" << endl;
-		cout << " |        WYBIERZ RODZAJ DZIALANIA        |" << endl;
-		cout << " |            1 - Dodawanie               |" << endl;
-		cout << " |            2 - Odejmowanie             |" << endl;
-		cout << " |            3 - Mnozenie                |" << endl;
-		cout << " |            4 - Dzielenie               |" << endl;
-		cout << " |             0 - Wyjscie                |" << endl;
+		menu();
 
 		cout << endl;
 		wybor = _getch();
 
-		switch (wybor)
-		{
-		case '1':
-			cout << "Wynik to: " << x + y << endl;
-			break;
+		wybory();
 
-		case '2':
-			cout << "Wynik to: " << x - y << endl;
-			break;
-
-		case '3':
-			cout << "Wynik to: " << x * y << endl;
-			break;
-
-		case '4':
-			cout << "Wynik to: " << x / y << endl;
-			break;
-
-		case '0':
-			a = 2;
-			break;
-
-		default:
-			cout << "Wpisales zly numer" << endl;
-		}
 		_getch();
 		system("cls");
-		
 	}
-
 }
 
-/*
-już chuj, że wszystko w mainie, już chuj, że getchar i edit z tym stringiem już nic
-ale kurwa ta nieskończona pętla XDDDDDD
-co ty miałeś w głowie oluś
-*/
