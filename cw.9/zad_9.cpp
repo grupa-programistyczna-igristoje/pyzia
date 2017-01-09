c1e0ac97e7ee6153f15d4d5711957b1127e0b19e
likearockets <aczech81@gmail.com>
2017-01-09 20:33:57 +0100

    Create zad_9.cpp

---
+++ cw.9/zad_9.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <conio.h>					
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+float x, y;
+int wybor;
+
+
+int main()
+{
+	for (;;)              //nieskonczona petla, dopoki wybor = 0 
+	{
+		cout << "Wybierz 1 liczbe: ";
+		cin >> x;
+		cout << "Wybierz 2 liczbe: ";
+		cin >> y;
+
+		cout << " | Kalkulator autorstwa Aleksandra Czecha |" << endl;
+		cout << " |        WYBIERZ RODZAJ DZIALANIA        |" << endl;
+		cout << " |            1 - Dodawanie               |" << endl;
+		cout << " |            2 - Odejmowanie             |" << endl;
+		cout << " |            3 - Mnozenie                |" << endl;
+		cout << " |            4 - Dzielenie               |" << endl;
+		cout << " |             0 - Wyjscie                |" << endl;
+
+		cout << endl;
+		wybor = _getch();                 //nie wywala juz przy znakach
+
+		switch (wybor)                    //uzylem case, bo widzialem nie raz u kogos i nie umialem jeszcze 
+		{
+		case '1':                         
+			cout << "Wynik to: " << x + y << endl;
+			break;
+
+		case '2':
+			cout << "Wynik to: " << x - y << endl;
+			break;
+
+		case '3':
+			cout << "Wynik to: " << x * y << endl;
+			break;
+
+		case '4':
+			cout << "Wynik to: " << x / y << endl;
+			break;
+
+		case '0':
+			exit(0);
+			break;
+
+		default:
+			cout << "Wpisales zly numer" << endl;
+		}
+		_getch();
+		system("cls");              //czysci konsole i mozna podawac nowe liczby 
+		}
+}
+
